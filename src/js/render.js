c426092b6c092bd3ac65a34fc5e53e3f30292fcd
gabs712 <gabs712.g@gmail.com>
2024-07-25 23:51:44 -0300

    Add render module

---
+++ src/js/render.js
@@ -0,0 +1,63 @@
+const WeatherTemplate = (weatherInfo, gifUrl, unit) => {
+  const weatherInfoDiv = document.querySelector('[data-weather-info-div]')
+
+  const getTemplate = () => {
+    const unitSymbol = unit === 'fahrenheit' ? 'ºF' : 'ºC'
+
+    const template = `
+      <div class="mt-3 text-white">
+        <div class="flex min-h-[1lh] justify-between gap-5">
+          <h2
+            data-address
+            class="bg col-span-12 text-base font-bold leading-tight"
+          >
+            ${weatherInfo.address}
+          </h2>
+          <button
+            class="grid size-6 shrink-0 place-content-center rounded-full bg-cyan-500 text-xs font-bold tracking-wider shadow hover:bg-slate-100 hover:text-cyan-500"
+          >
+            ºF
+          </button>
+        </div>
+        <div class="mt-4 flex justify-center gap-5">
+          <div data-temp class="min-h-[1lh] text-7xl tracking-tighter">
+            ${weatherInfo.temp}<span data-unit class="align-top text-xl tracking-wider">${unitSymbol}</span>
+          </div>
+          <div>
+            <img
+              class="size-12"
+              data-icon
+              src="assets/weather-icons/${weatherInfo.icon}.png"
+              alt=""
+            />
+          </div>
+        </div>
+        <div class="mt-5 flex justify-center">
+          <img
+            class="size-32 rounded-3xl bg-slate-100/90 object-cover object-center"
+            src="${gifUrl}"
+            alt=""
+          />
+        </div>
+        <div class="mt-3">
+          <h3 data-condition class="min-h-[1lh] font-bold leading-tight">
+            ${weatherInfo.condition}
+          </h3>
+          <p data-description class="mt-1.5 min-h-[1lh] text-base leading-snug">
+            ${weatherInfo.description}
+          </p>
+        </div>
+      </div>
+    `
+    return template
+  }
+
+  const render = () => {
+    weatherInfoDiv.innerHTML = ''
+    weatherInfoDiv.append(getTemplate())
+  }
+
+  return { render }
+}
+
+export default WeatherTemplate
